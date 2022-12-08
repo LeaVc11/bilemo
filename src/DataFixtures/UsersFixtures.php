f34a88e6d5baa002844ad96cf49e5eca8f720ef6
vcarine <you@example.com>
2022-12-08 17:53:21 +0100

    update UserFixture with library faker

--- src/DataFixtures/UsersFixtures.php
+++ src/DataFixtures/UsersFixtures.php
@@ -3,35 +3,46 @@
 namespace App\DataFixtures;
 
 use App\Entity\User;
+
+
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Faker;
 
 class UsersFixtures extends Fixture
 {
+
     private UserPasswordHasherInterface $hasher;
+    private Faker\Generator $faker;
 
     public function __construct(UserPasswordHasherInterface $hasher)
     {
         $this->hasher = $hasher;
+
     }
     public function load(ObjectManager $manager)
     {
-        $user1 = new User();
-        $user1->setEmail("admin@admin.admin");
-        $user1->setCompanyName('My Company');
-        $user1->setRoles(["ROLE_ADMIN"]);
-        $user1->setPassword($this->hasher->hashPassword($user1, "admin"));
+        $faker = Factory::create('fr_FR');
+        for ($i = 0; $i < 25; $i++) {
+            $user1 = new User();
+            $user1->setEmail($faker->email());
+            $user1->setCompanyName($faker->company());
+            $user1->setRoles(["ROLE_ADMIN"]);
+            $user1->setPassword($this->hasher->hashPassword($user1, "admin"));
 
-        $user2 = new User();
-        $user2->setEmail("user@user.user");
-        $user2->setCompanyName('Test Company');
-        $user2->setRoles(["ROLE_USER"]);
-        $user2->setPassword($this->hasher->hashPassword($user1, "user"));
+            $manager->persist($user1);
+        }
+        for ($i = 0; $i < 25; $i++) {
+            $user2 = new User();
+            $user2->setEmail($faker->email());
+            $user2->setCompanyName($faker->company());
+            $user2->setRoles(["ROLE_USER"]);
+            $user2->setPassword($this->hasher->hashPassword($user2, "user"));
 
-
-        $manager->persist($user1);
-        $manager->persist($user2);
+            $manager->persist($user2);
+        }
         $manager->flush();
     }
 }
