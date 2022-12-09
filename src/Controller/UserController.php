4121644bc4ed2da167926992eb1100c2cdeaf604
vcarine <you@example.com>
2022-12-09 16:03:44 +0100

    update entity user create user deletion

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,9 +4,12 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
 
@@ -21,10 +24,33 @@ class UserController extends AbstractController
         $jsonUserList = $serializer->serialize($userList, 'json', ['groups' => 'getUsers']);
         return new JsonResponse($jsonUserList, Response::HTTP_OK, [], true);
     }
-    #[Route('/{id}', name: 'detailUser', methods: ['GET'])]
+
+    #[Route('', name: 'createUser', methods: ['POST'])]
+    public function createUser(Request $request, SerializerInterface $serializer, EntityManagerInterface $em, UserPasswordHasherInterface $userPasswordHasher): JsonResponse
+    {
+        $user = $serializer->deserialize($request->getContent(), User::class, 'json');
+
+        $em->persist($user);
+        $em->flush();
+
+        $jsonUser = $serializer->serialize($user, 'json', ['groups' => 'getUsers']);
+
+
+        return new JsonResponse($jsonUser, Response::HTTP_CREATED, [], true);
+    }
+
+    #[Route('/edit/{id}', name: 'detailUser', methods: ['GET'])]
     public function getDetailUser(User $user, SerializerInterface $serializer): JsonResponse
     {
         $jsonUser = $serializer->serialize($user, 'json', ['groups' => 'getUsers']);
         return new JsonResponse($jsonUser, Response::HTTP_OK, [], true);
+    }
+
+    #[Route('/delete/{id}', name: 'deleteUser', methods: ['DELETE'])]
+    public function DeleteUser(User $user, EntityManagerInterface $em): JsonResponse
+    {
+        $em->remove($user);
+        $em->flush();
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
     }
 }
