bbebf1272cc780708e212b65d378e0eddb130888
vcarine <you@example.com>
2022-12-09 15:29:12 +0100

    update user

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,10 +4,13 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Serializer\SerializerInterface;
 
 #[Route('/api/users', name: 'user_')]
@@ -21,7 +24,19 @@ class UserController extends AbstractController
         $jsonUserList = $serializer->serialize($userList, 'json', ['groups' => 'getUsers']);
         return new JsonResponse($jsonUserList, Response::HTTP_OK, [], true);
     }
-    #[Route('/{id}', name: 'detailUser', methods: ['GET'])]
+    #[Route('', name: 'createUser', methods: ['POST'])]
+    public function createUser(Request $request, SerializerInterface $serializer, EntityManagerInterface $em): JsonResponse
+    {
+        $user = $serializer->deserialize($request->getContent(), User::class, 'json');
+        $em->persist($user);
+        $em->flush();
+
+        $jsonUser = $serializer->serialize($user, 'json', ['groups' => 'getProducts']);
+
+
+        return new JsonResponse($jsonUser, Response::HTTP_CREATED,[],true);
+    }
+    #[Route('/edit/{id}', name: 'detailUser', methods: ['GET'])]
     public function getDetailUser(User $user, SerializerInterface $serializer): JsonResponse
     {
         $jsonUser = $serializer->serialize($user, 'json', ['groups' => 'getUsers']);
