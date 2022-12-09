e74ac578086ee44b58801bc82ba971ba3b704765
vcarine <you@example.com>
2022-12-09 14:29:47 +0100

    consult the list and the details of the products

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -4,8 +4,10 @@ namespace App\Controller;
 
 use App\Entity\Product;
 use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
@@ -20,10 +22,18 @@ class ProductController extends AbstractController
         $jsonProductList = $serializer->serialize($productList, 'json');
         return new JsonResponse($jsonProductList, Response::HTTP_OK, [], true);
     }
-    #[Route('/{id}', name: 'app_product' , methods: ['GET'])]
+    #[Route('/{id}', name: 'detailProduct' , methods: ['GET'])]
     public function getProductDetail(Product $product, SerializerInterface $serializer): JsonResponse
     {
         $jsonProduct = $serializer->serialize($product, 'json');
         return new JsonResponse($jsonProduct, Response::HTTP_OK, [], true);
     }
+    #[Route('/{id}', name: 'deleteProduct' , methods: ['DELETE'])]
+    public function DeleteProduct(Product $product, EntityManagerInterface $em): JsonResponse
+    {
+        $em->remove($product);
+        $em->flush();
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
+    }
+
 }
