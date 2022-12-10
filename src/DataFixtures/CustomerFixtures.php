f6cf802d24d7418890243f34ba92247913772298
vcarine <you@example.com>
2022-12-10 19:26:23 +0100

    update

--- src/DataFixtures/CustomerFixtures.php
+++ src/DataFixtures/CustomerFixtures.php
@@ -3,26 +3,23 @@
 namespace App\DataFixtures;
 
 use App\Entity\Customer;
-use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
-use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
 
-class CustomerFixtures extends Fixture implements DependentFixtureInterface
+class CustomerFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
 
         $faker = Factory::create('fr_FR');
-        for ($i = 0; $i <= 20; $i++) {
+        for ($i = 0; $i < 20; $i++) {
             $customer = (new Customer())
                 ->setName($faker->name())
                 ->setEmail($faker->email());
+            $manager->persist($customer);
         }
-        $this->addReference(UsersFixtures::USER_REFERENCE . $i, $customer);
 
-        $manager->persist($customer);
         $manager->flush();
     }
     public function getDependencies(): array
